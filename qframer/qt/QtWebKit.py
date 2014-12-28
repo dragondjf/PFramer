baba771691ffb6938f8f6e80d99bd89e41d75037
dragondjf <dragondjf@gmail.com>
2014-12-28 19:43:15 +0800

    add support qtwebkit for qframer.qt

---
+++ qframer/qt/QtWebKit.py
@@ -0,0 +1,16 @@
+
+"""
+Provides QtWebKit classes and functions.
+"""
+import os
+from . import QT_API
+from . import PYQT5_API
+from . import PYQT4_API
+from . import PYSIDE_API
+
+if os.environ[QT_API] == PYQT5_API:
+    from PyQt5.QtWebKit import *
+elif os.environ[QT_API] == PYQT4_API:
+    from PyQt4.QtWebKit import *
+elif os.environ[QT_API] == PYSIDE_API:
+    from PySide.QtWebKit import *
